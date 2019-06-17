45ad4ddad829d55833cb1845e0b74d12bf302659
alex <1069076193@qq.com>
2019-06-17 21:30:17 +0800

    初次提交

---
+++ course/assets/js/statistics.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?6faa28aba24f26de59ff58f5e0c550c5";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
